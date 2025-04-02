d1758a12fac005dca1b8febb2a0db6d024caa1da
baldbugulma <ForZaArindal@gmail.com>
2025-04-02 15:34:05 +0300

    resize scan

--- src/app/common-ui/scanner/qr-scanner/qr-scanner.component.ts
+++ src/app/common-ui/scanner/qr-scanner/qr-scanner.component.ts
@@ -36,7 +36,7 @@ export class QrScannerComponent implements AfterViewInit{
       { facingMode: "environment" }, // Используем заднюю камеру
       {
         fps: 15,
-        qrbox: { width: 250, height: 250 } // Размер области сканирования
+        qrbox: { width: 200, height: 200 } // Размер области сканирования
       },
       (decodedText) => this.onScanSuccess(decodedText),
       (errorMessage) => this.onScanFailure(errorMessage)
